9630fae0727093304420506090854ea1753268f8
Yohannes Mesganaw <68536682+yohannesME@users.noreply.github.com>
2022-02-09 11:21:02 +0900

    the sign in and login pages

---
+++ Login/app.js
@@ -0,0 +1,7 @@
+const email = document.getElementById("login-email")
+const password = document.getElementById("login-password")
+const submitbtn = document.getElementById("submit")
+
+submitbtn.addEventListener('click', (e)=>{
+    e.preventDefault();
+})
